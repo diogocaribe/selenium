b0da8be72cc95350447383a66d3b13b0ce35df87
Diogo Caribé <dogosousa@gmail.com>
2025-03-31 10:02:34 -0300

    first commit

---
+++ docker-run.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+msg_usage='Usage: docker-run.sh --<option>\n\nOptions: \n\t--local (local development)\n\t--server (dev/stag/prod server)\n'
+
+if [ $# -ne 1 ]; then
+    echo ${msg_usage}
+    exit 1
+else
+    case "$1" in
+        --local) compose_file='compose.yml'
+            ;;
+        --server) compose_file='compose.server.yml'
+            ;;
+        --*) echo "bad option: '$1'\n"
+            ;;
+        *) echo "bad option: '$1'\n"
+            ;;
+    esac
+
+    if [ -z ${compose_file+x} ]; then
+        echo ${msg_usage}
+    else
+        if [ ! -f ".env" ]; then
+            echo "ERROR: File not found 'docker.env'. Exiting..."
+            exit 1
+        fi
+
+        . ./.env 
+        export $(grep --regexp ^[A-Z] .env | cut -d= -f1)
+        docker network ls | grep ${NETWORK} || docker network create ${NETWORK}
+        docker stop ${CONTAINER_NAME}
+        docker rm ${CONTAINER_NAME}
+        docker compose --env-file .env --file ${compose_file} --compatibility up --detach --force-recreate --build --remove-orphans
+    fi
+fi
+
+exit 0
